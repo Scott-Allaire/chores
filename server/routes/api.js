3da52e4e868f7a0f0de1d04f5ad38736c08e0f96
Scott Allaire <sallaire4@gmail.com>
2016-11-28 19:52:38 -0600

    Exploded chore API

--- server/routes/api.js
+++ server/routes/api.js
@@ -6,10 +6,77 @@ var Chore = require('../models/chore');
 var Event = require('../models/event');
 
 // Routes
-Chore.methods(['get','put','post','delete']);
-Chore.register(router, '/chores');
+// Chore.methods(['get','put','post','delete']);
+// Chore.register(router, '/chores');
+router.route('/chores')
+    .get(function (req, res) {
+        Chore.find()
+            .sort({ name: 1 })
+            .exec(function (err, chores) {
+                if (err)
+                    res.send(err);
 
-Event.methods(['get','put','post','delete']);
+                res.json(chores);
+            });
+    })
+    .post(function (req, res) {
+
+        var chore = new Chore();
+        chore.name = req.body.name;
+        chore.description = req.body.description;
+        chore.frequency = req.body.frequency;
+        chore.nextDue = req.body.nextDue;
+
+        Chore.create(chore, function (err, chore) {
+            if (err)
+                res.send(err);
+
+            res.json(chore);
+        });
+
+    });
+
+router.route('/chores/:chore_id')
+    .get(function (req, res) {
+        Chore.findById(req.params.chore_id, function (err, chore) {
+            if (err)
+                res.send(err);
+            res.json(chore);
+        });
+    })
+    .put(function (req, res) {
+        Chore.findById(req.params.chore_id, function (err, chore) {
+
+            if (err)
+                res.send(err);
+
+            chore.name = req.body.name;
+            chore.description = req.body.description;
+            chore.frequency = req.body.frequency;
+            chore.nextDue = req.body.nextDue;
+
+            // save the bear
+            chore.save(function (err) {
+                if (err)
+                    res.send(err);
+
+                res.json(chore);
+            });
+
+        });
+    })
+    .delete(function (req, res) {
+        Chore.remove({
+            _id: req.params.chore_id
+        }, function (err, chore) {
+            if (err)
+                res.send(err);
+
+            res.json(chore);
+        });
+    });
+
+Event.methods(['get', 'put', 'post', 'delete']);
 Event.register(router, '/events');
 
 // Return router
